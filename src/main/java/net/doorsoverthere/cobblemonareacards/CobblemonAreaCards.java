8113c6e96ef0f05494465f6e0b48af1654ffdba9
adwyer02 <aidandyr@gmail.com>
2025-04-02 13:29:30 -0500

    Initial commit

---
+++ src/main/java/net/doorsoverthere/cobblemonareacards/CobblemonAreaCards.java
@@ -0,0 +1,17 @@
+package net.doorsoverthere.cobblemonareacards;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CobblemonAreaCards implements ModInitializer {
+	public static final String MOD_ID = "cobblemonareacards";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/doorsoverthere/cobblemonareacards/CobblemonAreaCardsClient.java
@@ -0,0 +1,10 @@
+package net.doorsoverthere.cobblemonareacards;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CobblemonAreaCardsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
